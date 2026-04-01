100ce3e0275184de89af8f7cefcd14a59628f3a0
yuenanmu <1730593502@qq.com>
2026-04-01 09:21:13 +0800

    写地址路由

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello, FastAPI!"}
 
-@app.get("/learn")
+@app.get("/learn")#路由：地址与函数映射关系
 async def learn_fastapi():
     return {"msg":"我正在学习FastAPI！"}
+
+@app.get("/book/{book_id}")
+async def return_book(book_id:int):
+    return {"book_id":f"这是我的第{book_id}本书"}
