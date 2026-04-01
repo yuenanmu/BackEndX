3afd8ca170dbe413a503310e22f25db0ca682e0e
yuenanmu <1730593502@qq.com>
2026-04-01 16:18:27 +0800

    路径参数test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app = FastAPI()
 
@@ -10,6 +10,15 @@ async def read_root():
 async def learn_fastapi():
     return {"msg":"我正在学习FastAPI！"}
 
-@app.get("/book/{book_id}")
-async def return_book(book_id:int):
+#路径参数Path Parameters：在URL路径中定义参数，使用{}括起来，函数参数与路径参数名称一致
+@app.get("/book_num/{book_id}")
+async def get_book(book_id:int):
     return {"book_id":f"这是我的第{book_id}本书"}
+@app.get("/book_details/{book_id}")
+async def get_book_details(book_id:int=Path(...,title="书籍ID",description="这是书籍的ID，必须是整数,范围1~100",gt=0,lt=101)):
+    return {"book_id":f"这是第{book_id}本书的详细信息"}
+@app.get("/author/{author_name}")
+async def get_author(author_name:str=Path(...,description="这是作者的名字，必须是字符串",min_length=2,max_length=50)):
+    return {"author_name":f"这是作者{author_name}的详细信息"}
+
+
