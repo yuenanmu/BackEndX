8cdc098120f3f590842f2bd0d807cb867ae40011
yuenanmu <1730593502@qq.com>
2026-04-11 10:09:36 +0800

    通过添加CORS中间件，解决跨域问题

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app_backend.routers import news
+from fastapi.middleware.cors import CORSMiddleware
 # import traceback
 # import sys
 
@@ -11,8 +12,24 @@ from app_backend.routers import news
 # app = FastAPI(debug=True)
 app = FastAPI()
 
+#添加CORS中间件，解决跨域问题：协议、端口、域名都要一致才允许
+origins=[
+    "http://localhost:3000",
+    "http://localhost:8000",
+    ""
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源,实际应用时，需要自定义允许的源列表
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有HTTP请求方法：request,post,put,delete等
+    allow_headers=["*"],  # 允许所有HTTP头
+)
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI!"}
 #挂载模块化的路由（各个模块的接口）
 app.include_router(news.router)
+#运行指令
+# uvicorn main:app --reload
